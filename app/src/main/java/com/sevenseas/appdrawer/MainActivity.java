65f9ec6fa68363143af6020983b4543eb410756e
rupeshjadhav <myprivateproject@outlook.com>
2020-10-28 13:36:35 +0530

    Initial commit...

---
+++ app/src/main/java/com/sevenseas/appdrawer/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sevenseas.appdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.sevenseas.appdrawer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        toggle = new ActionBarDrawerToggle(this, binding.drawerlayout, R.string.open, R.string.close);
+        binding.drawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
